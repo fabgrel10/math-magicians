7b05c263a360d560b0e5a3594b17f56546e82213
Fabricio Garcia <fabgrel10@gmail.com>
2022-01-05 13:03:10 -0600

    ♻️ refactor: Calculator class based componte into functional component
    I also modified Display component to eliminate showResult function and
    pass state components as props directly.

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -2,18 +2,28 @@ import PropTypes from 'prop-types';
 
 import styles from './Display.module.css';
 
-const Display = ({ result }) => (
+const Display = ({ total, operation, next }) => (
   <div className={styles.display} mode="single" max={70}>
-    <p>{result}</p>
+    <p>{next || operation || total || 0}</p>
   </div>
 );
 
 Display.propTypes = {
-  result: PropTypes.string,
+  total: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  operation: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func,
+  ]),
+  next: PropTypes.string,
 };
 
 Display.defaultProps = {
-  result: '0',
+  total: 0,
+  operation: null,
+  next: null,
 };
 
 export default Display;
