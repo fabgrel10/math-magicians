9a3536a4ed0e9c23ab0eecc84a012f06252dcd05
Fabricio Garcia <fabgrel10@gmail.com>
2022-01-12 17:51:05 -0600

    🧪 Add Jest snaptshot to test Calculator component

--- src/tests/components/Calculator.test.js
+++ src/tests/components/Calculator.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Display from '../../components/Calculator/Display/Display';
 import Button from '../../components/Calculator/Button/Button';
 import ButtonBox from '../../components/Calculator/ButtonBox/ButtonBox';
 import CalculatorFrame from '../../components/Calculator/CalculatorFrame/CalculatorFrame';
+import Calculator from '../../components/Calculator/Calculator';
 
 describe('components', () => {
   it('test Display components renders correctly', () => {
@@ -21,5 +23,10 @@ describe('components', () => {
   it('test CalculatorFrame components renders correctly', () => {
     const { queryByTitle } = render(<CalculatorFrame />);
     expect(queryByTitle('calculator-frame-title')).toBeTruthy();
+  });
+
+  it('Calculator component is working', () => {
+    const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
